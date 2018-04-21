8aef462ebcf18fc563778d4297c53cbcf302052e
Gabriel Barzola <barcho@gmail.com>
2018-04-21 11:49:12 -0300

    proyecto inicial

---
+++ src/Inicio/Workshop/NewPayment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Workshop
+{
+    public class NewPayment
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public bool Valid { get; set; }
+        public string Transaction { get; set; }
+    }
+}
